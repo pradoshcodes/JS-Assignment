2814ecab51d688a90289f9feeaf7abbe5d950677
pradoshcodes <pradoshmohanty034@gmail.com>
2024-03-24 17:44:13 +0530

    Assignment-9

---
+++ que9.js
@@ -0,0 +1,4 @@
+let num1=24
+let num2=56
+let num3=98;
+num1 < num2 ? (num1 < num3 ?  console.log(`least number:${num1}`) : console.log(`least number:${num3}`)) : (num2 < num3 ? console.log(`least number:${num2}`) : console.log(`least number:${num3}`));
