26c7b7e9db29290276e9729ad10c42400a8ae025
pradoshcodes <pradoshmohanty034@gmail.com>
2024-03-24 17:03:17 +0530

    Assignment-4

---
+++ que4.js
@@ -0,0 +1,8 @@
+let num=[343,12,543,7676,8]
+for(arr of num){
+    if(arr>99 && arr<1000){
+        console.log(`Number is 3-digit:${arr}`);
+    }else{
+        console.log(`Number is not 3-digit:${arr}`);
+    }
+}
