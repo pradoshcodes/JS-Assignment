45114f4845296f99c249aa0fa261bc8029d99523
pradoshcodes <pradoshmohanty034@gmail.com>
2024-03-24 17:07:56 +0530

    Assignment-5

---
+++ que5.js
@@ -0,0 +1,8 @@
+let num=[47,64,56,89,90,24,9,4,2]
+for(arr of num){
+    if(arr %2==0) {
+        console.log(`Number is even:${arr}`);
+    }else{
+        console.log(`Number is odd:${arr}`);
+    }
+}
