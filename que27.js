74d1402efee58fefe75457e995890f53e00e963b
pradoshcodes <pradoshmohanty034@gmail.com>
2024-03-25 11:04:58 +0530

    Assignment-27

---
+++ que27.js
@@ -0,0 +1,13 @@
+let size =[4,8,12,16,20,24,28,32,36,40]
+for(arr  of size){
+    while(arr % 4 ==0){
+        console.log(`${arr} is divisible by 4`)
+      arr++  
+    }
+    
+}
+
+// let num = [5,6,7,8]
+// for (i in num) {
+//     console.log(`The value at index ${i} is ${num[i]}`)
+// }  show  the index of a given array
