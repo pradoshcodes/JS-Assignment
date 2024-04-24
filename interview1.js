0d52c0d0af0824911ba660b1f4e9e9628c3e4fc9
pradoshcodes <pradoshmohanty034@gmail.com>
2024-04-24 22:58:44 +0530

    js interview question

---
+++ interview1.js
@@ -0,0 +1,11 @@
+// 3. Write a JavaScript function to check if a given string is a palindrome (reads the same forwards and backwards). 
+
+// The interviewer is looking for the candidate’s familiarity with loop constructs, JavaScript string methods, and other basic JavaScript syntax. They will evaluate the candidate’s skills based on the approach used to solve the palindrome problem. 
+
+// Sample answer: 
+
+function isPalindrome(str) { 
+
+  return str === str.split(”).reverse().join(”); 
+
+}
