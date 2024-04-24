760eba0fc251275d9e5fcca3a0721f6a086987ce
pradoshcodes <pradoshmohanty034@gmail.com>
2024-04-24 23:00:25 +0530

    js interview question

---
+++ interview2.js
@@ -0,0 +1,7 @@
+// 4. Write a JavaScript program to reverse a given string. 
+
+// Hiring managers are expecting an accurate solution that demonstrates the interviewee’s proficiency in JavaScript programming.  
+
+// Sample answer: 
+
+const reverseString = (str) => str.split(”).reverse().join(”); 
