b8c499db296bab787ee20680bd161d17c5ffb7e7
pradoshcodes <pradoshmohanty034@gmail.com>
2024-04-24 22:56:22 +0530

    js interview question

---
+++ interview.js
@@ -0,0 +1,12 @@
+// 2. Write a JavaScript program to find the maximum number in an array. 
+
+// A hiring manager asks this question to analyze the candidate’s ability to write clear and efficient code. It’s crucial for candidates to explain the code step-by-step while demonstrating bug-free code. 
+
+// Sample answer: 
+
+function findMaxNumber(arr) { 
+
+  return Math.max(…arr); 
+
+} 
+findMaxNumber();
