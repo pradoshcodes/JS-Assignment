685a2505bf715c1ea57e168bdeb67e818b0ab17f
pradoshcodes <pradoshmohanty034@gmail.com>
2024-04-29 12:58:17 +0530

    factorial of a number

---
+++ interview4.js
@@ -0,0 +1,17 @@
+let num=-5;
+function fun(){
+    let fact=1
+    if(num<0) 
+    return "undefined";
+    else if(num==0 || num==1)
+        return 1;
+    
+        else
+        
+        for(let i=2;i<=num;i++)
+            fact *= i;
+        return fact;
+    
+
+}
+console.log("The factorial of "+num+" is: "+fun());
