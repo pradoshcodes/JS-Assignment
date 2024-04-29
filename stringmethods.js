92598c2a9f9db41e85f7ae32e4aa88f8b5df8ccc
pradoshcodes <pradoshmohanty034@gmail.com>
2024-04-29 11:24:53 +0530

    string methods example

---
+++ stringmethods.js
@@ -0,0 +1,52 @@
+//length()
+//---------------
+
+let text="hello";
+console.log(text.length);
+
+
+//toUpperCase() & toLowerCase()
+//----------------------------------
+let a="hello";
+let b="JAVASCRIPT";
+console.log(a.toUpperCase());
+console.log(b.toLowerCase());
+
+
+//includes()
+//------------
+let c= "Hello, how are you?";
+console.log(c.includes("you"));  
+console.log(c.includes("why"));  
+
+//startsWith(), endsWith() 
+//-------------------------
+let d = "Hello, I am John Doe.";
+console.log(d.startsWith("Hello"));    //true
+console.log(d.endsWith("."));         //true
+
+//search()
+//----------
+let e = "Please locate where 'locate' occurs!";
+console.log(e.search("locate"));
+
+//match()
+//--------
+let f = "The rain in SPAIN stays mainly in the plain";
+console.log(f.match(/plain/));//regular expression
+
+//indexOf()
+//--------
+let g = "Please locate where 'locate' occurs!";
+console.log( g.indexOf("locate"));
+
+//lastIndexOf()
+//--------------
+let h = "Hello planet earth, you are a great planet.";
+console.log( h.lastIndexOf("planet"));
+
+//trim()
+//----------
+let i = "\n\t Hello world \n\t ";
+console.log(i);
+console.log(i.trim());
